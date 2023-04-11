2ea45442e50be9e3668f978bbb9352016c904331
Morris Alromhein <Morris4028@gmail.com>
2023-04-10 22:51:15 -0400

    Added Basics of Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Product } from '../components/Product';
+import { ShopContext } from '../context/ShopContext';
+import { PRODUCTS } from '../Products';
 
 const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
   return (
-    <div> This is the cart </div>
-  )
-}
+    <div className='cart'>
+      <div>
+        <h1> Your Cart Items</h1>
+      </div>
+      <div className='cartItems'>
+        {PRODUCTS.map((Product) => {
+          if (cartItems[Product.id] > 0) {
+            return <CartItem data={Product} />;
+          }
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
