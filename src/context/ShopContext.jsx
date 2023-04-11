caa7521b7457a6ff01e2d0dbcd0b2f9ca658febf
Morris Alromhein <Morris4028@gmail.com>
2023-04-11 00:06:30 -0400

    Added subtotal

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -14,6 +14,17 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
+
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
   };
@@ -31,6 +42,7 @@ export const ShopContextProvider = (props) => {
     addToCart,
     removeFromCart,
     updateItemCartAmount,
+    getTotalCartAmount
   };
 
   return (
