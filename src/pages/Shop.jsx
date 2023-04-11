968b90abce5ff25e65dd2917e5bc781388b9b9b6
Morris Alromhein <Morris4028@gmail.com>
2023-04-10 22:39:05 -0400

    Added Shop Page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
+import './Shop.css'
+import { Product } from '../components/Product';
+import { PRODUCTS } from '../Products';
 
 const Shop = () => {
-  return <div> This is the shop / landing page </div>;
+  return (
+    <div className='shop'>
+      <div className='shop-title'>
+        <h1>We Skii Rental Shop</h1>
+      </div>
+      <div className='products'>
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-/*
- * Create landing page
- * Search bar
- * images  list
- * each p
- *
- *
- *
- */
-
 export default Shop;
+
