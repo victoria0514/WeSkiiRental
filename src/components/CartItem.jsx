892084192821ed1a4530ea2d48408957460e41ff
Morris Alromhein <Morris4028@gmail.com>
2023-04-11 00:02:25 -0400

    Added CartItem Component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import { Button, Input } from '@mui/material';
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart, updateItemCartAmount } =
+    useContext(ShopContext);
   return (
     <div className='cartItem'>
       <img src={productImage} />
@@ -11,6 +15,37 @@ export const CartItem = (props) => {
           <b> {productName}</b>
         </p>
         <p> ${price}</p>
+        <div className='countHandler'>
+          <Button
+            variant='outlined'
+            color='primary'
+            size='small'
+            onClick={() => removeFromCart(id)}
+          >
+            {' '}
+            -{' '}
+          </Button>
+
+          <Input
+      size='small'
+            value={cartItems[id]}
+            onChange={(e) => {
+              updateItemCartAmount(Number(e.target.value), id);
+            }}
+          />
+
+          <Button
+            variant='outlined'
+            color='primary'
+            size='small'
+            onClick={() => {
+              addToCart(id);
+            }}
+          >
+            {' '}
+            +{' '}
+          </Button>
+        </div>
       </div>
     </div>
   );
