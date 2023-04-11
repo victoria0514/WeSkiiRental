b485122870e0d467db53d447e541e4fd14ac206a
Morris Alromhein <Morris4028@gmail.com>
2023-04-10 22:39:35 -0400

    Added Shop Name in top left

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ShoppingCart } from '@mui/icons-material';
+import icon from '../icons/WeSkiiLogo.png';
 import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <div className='links'>
-        <Link to='/Login'> Login </Link>
-        <Link to='/'> Shop </Link>
-        <Link to='/Cart'>
-          <ShoppingCart size={24} weight='fill' color='white' />
+    <>
+      <div className='navbar'>
+        <div className='info'>
+          <h2>Fantastic⁴ Store</h2>
+        </div>
+
+        <div className='links'>
+          <Link to='/Login'> Login </Link>
+          <Link to='/'> Shop </Link>
+          <Link to='/Cart'>
+            <ShoppingCart size={24} weight='fill' color='white' />
         </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
