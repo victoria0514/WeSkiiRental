e93acf570d6057e32662d54b7de529f027224296
Morris Alromhein <Morris4028@gmail.com>
2023-04-10 22:51:06 -0400

    Changed Button Styling

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material';
 import React, { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
-  const { addToCart } = useContext(ShopContext);
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const cartItemAmount = cartItems[id];
   return (
     <div className='product'>
       <img src={productImage} alt={productName} />
@@ -13,10 +15,9 @@ export const Product = (props) => {
         </p>
         <p> ${price}</p>
       </div>
-      <button className='addToCartBttn' onClick={() => addToCart(id)}>
-        {' '}
-        Add to Cart{' '}
-      </button>
+      <Button variant='outlined' color='primary' onClick={() => addToCart(id)}>
+        Add to Cart{cartItemAmount > 0 && <> ({cartItemAmount})</>}
+      </Button>
     </div>
   );
 };
