6d5735a5b3fe1b3d6664c6d9ff01a218033064a4
Morris Alromhein <Morris4028@gmail.com>
2023-04-28 00:31:40 -0400

    Added Account to the navbar and Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginForm from './pages/LoginForm';
 import Signup from './pages/Signup';
 import Shop from './pages/Shop';
 import Cart from './pages/Cart';
+import Account from './pages/Account';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShopContextProvider } from './context/ShopContext';
@@ -18,6 +19,7 @@ function App() {
             <Route path='/login' element={<LoginForm />} exact />
             <Route path='/signup' element={<Signup />} exact />
             <Route path='/cart' element={<Cart />} exact />
+            <Route path='/account' element={<Account />} exact /> 
           </Routes>
         </BrowserRouter>
       </ShopContextProvider>
