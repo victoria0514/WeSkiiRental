2e89cb32737db59335b5dd6edfcbed816ce3845a
Morris Alromhein <Morris4028@gmail.com>
2023-04-28 02:04:01 -0400

    Added ability to modify user information

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { TextField, Button } from '@mui/material';
 import './Styling/LoginForm.css';
-import logo from '../icons/WeSkiiLogo.png';
 import { useNavigate } from 'react-router-dom';
+import ShippingInfo from '../components/ShippingInfo';
 
 const Account = () => {
   const navigate = useNavigate();
@@ -14,42 +14,86 @@ const Account = () => {
   const handlePasswordSubmit = (event) => {
     event.preventDefault();
     if (password === oldPassword) {
-      console.log('Password Changed');
+      alert('Password Changed');
+      sessionStorage.setItem('password', newPassword)
     } else {
-      console.log('Password Incorrect');
-      console.log(password);
+      alert('Password Incorrect');
     }
+  };
+
+  const handleSignOut = () => {
+    sessionStorage.setItem('isLoggedIn', 'false');
+    sessionStorage.setItem('password', '');
+    sessionStorage.setItem('username', '');
+    sessionStorage.setItem('firstName', '');
+    sessionStorage.setItem('lastName', '');
+    sessionStorage.setItem('address', '');
+    sessionStorage.setItem('city', '');
+    sessionStorage.setItem('state', '');
+    sessionStorage.setItem('zip', '');
+    navigate('/login');
   };
 
   const handleLoginClick = () => {
     navigate('/login');
   };
 
+  const handleDisplayInfo = () => {
+    console.log(sessionStorage.getItem('firstName'));
+    console.log(sessionStorage.getItem('lastName'));
+    console.log(sessionStorage.getItem('address'));
+    console.log(sessionStorage.getItem('city'));
+    console.log(sessionStorage.getItem('state'));
+    console.log(sessionStorage.getItem('zip'));
+    console.log(sessionStorage.getItem('password'));
+  };
+
+  const style_outer = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  };
+
   return (
     <div>
       {isLoggedIn === 'true' ? (
-        <div>
-          <h1> You are logged in</h1>
-          <TextField
-            id='outlined-basic'
-            label='Old Password'
-            variant='outlined'
-            value={oldPassword}
-            onChange={(e) => {
-              setOldPassword(e.target.value);
-            }}
-          />
-          <form onSubmit={handlePasswordSubmit}>
+        <div style={style_outer} className='outerContainer'>
+          <div className='changePasswordContainer'>
+            <h1>Change Password</h1>
             <TextField
-              id='outlined-basic'
-              label='New Password'
-              variant='outlined'
-              value={newPassword}
+              style={{ marginBottom: '20px' }}
+              id='standard'
+              label='Old Password'
+              variant='standard'
+              value={oldPassword}
               onChange={(e) => {
-                setNewPassword(e.target.value);
+                setOldPassword(e.target.value);
               }}
             />
-          </form>
+            <form onSubmit={handlePasswordSubmit}>
+              <TextField
+                id='standard'
+                label='New Password'
+                variant='standard'
+                value={newPassword}
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                }}
+              />
+            </form>
+          </div>
+          <div className='changeShippingContainer'>
+            <h1> Your shipping info is </h1>
+            <ShippingInfo />
+          </div>
+          <div>
+            <h1> Sign Out</h1>
+            <Button variant='contained' onClick={handleSignOut}>
+              Sign Out
+            </Button>
+            <Button variant='contained' onClick={handleDisplayInfo}> Display Info </Button>
+
+          </div>
         </div>
       ) : (
         <div>
