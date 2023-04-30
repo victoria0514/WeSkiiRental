b84ab9d1bd6e84837b80c750e5e24bb40709201a
Morris Alromhein <Morris4028@gmail.com>
2023-04-30 18:17:01 -0400

    Added Success page to Routes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Shop from './pages/Shop';
 import Cart from './pages/Cart';
 import Account from './pages/Account';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShopContextProvider } from './context/ShopContext';
 import NotFound from './pages/NotFound';
+import Success from './pages/Success';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path='/cart' element={<Cart />} exact />
             <Route path='/account' element={<Account />} exact />
             <Route path='/not-found' element={<NotFound />} exact />
+            <Route path='/Success' element={<Success />} exact />
             <Route path='/' element={<Shop />} exact />
             <Route path='*' element={<NotFound />} />
           </Routes>
---
+++ src/pages/Success.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Success = ({purchaseType, totalAmount}) => {
+  return (
+    <div>Success</div>
+  )
+}
+
+export default Success
