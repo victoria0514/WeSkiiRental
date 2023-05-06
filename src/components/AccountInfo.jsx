b4755acd39d078fdbcc070064cd380e59cb5dd46
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2023-05-05 20:24:41 -0400

    update

--- src/components/AccountInfo.jsx
+++ src/components/AccountInfo.jsx
@@ -1,27 +1,18 @@
-import React, { useState } from 'react';
-import './Styling/Title.css';
-import { TextField } from '@mui/material';
-import './Styling/AccountInfo.css';
-import {  createUserWithEmailAndPassword  } from 'firebase/auth';
-import {auth,db} from '../firebase';
-import { useNavigate,Link } from 'react-router-dom';
-import { collection, addDoc,doc } from "firebase/firestore";
+import React, { useState } from "react";
+import "./Styling/Title.css";
+import { Button, TextField } from "@mui/material";
+import "./Styling/AccountInfo.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const AccountInfo = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [state, setState] = useState('');
-  const [zip, setZip] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const onSubmit = async (e) => {
     // e.preventDefault()
-    const collectionRef = collection(db,"Shipping info")
-
     console.log(username);
     console.log(password);
     await createUserWithEmailAndPassword(auth, username, password)
@@ -39,30 +30,10 @@ const AccountInfo = () => {
       });
   };
 
-
-// ADD HERE
-     await addDoc(collectionRef,{
-        firstName: firstName, lastName:lastName, address:address, city:city, state:state, zip:zip,
-        email:auth.currentUser.email, id:auth.currentUser.uid
-      }
-
-    
-     ).then(()=>{
-
-      navigate("/login")
-     })
-     
-     .catch((error) =>{
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode, errorMessage);
-     })
-  }
   return (
-    <>
- 
-      <h3 className='title'>Login Info</h3>
-      <div className='outer'>
+    <div className="outer-outer">
+      <h3 className="title">Login Info</h3>
+      <div className="outer">
         <TextField
           id="outlined-basic"
           label="email"
@@ -81,84 +52,10 @@ const AccountInfo = () => {
           className="inner"
         />
       </div>
-
-
-  
-       
-   
-    <h3 className='title'> Shipping Info </h3>
-
-<div className='outer'>
-  <TextField
-    id='outlined-basic'
-    label='First Name'
-    variant='outlined'
-    value={firstName}
-    onChange={(e) => setFirstName(e.target.value)}
-    className='inner'
-  />
-  <TextField
-    id='outlined-basic'
-    label='Last Name'
-    variant='outlined'
-    value={lastName}
-    onChange={(e) => setLastName(e.target.value)}
-    className='inner'
-  />
-</div>
-
-<div className='outer'>
-  <TextField
-    id='outlined-basic'
-    label='Address'
-    variant='outlined'
-    value={address}
-    onChange={(e) => setAddress(e.target.value)}
-    className='inner'
-  />
-
-  <TextField
-    id='outlined-basic'
-    label='City'
-    variant='outlined'
-    value={city}
-    onChange={(e) => setCity(e.target.value)}
-    className='inner'
-  />
-</div>
-
-<div className='outer'>
-  <TextField
-    id='outlined-basic'
-    label='State'
-    variant='outlined'
-    value={state}
-    onChange={(e) => setState(e.target.value)}
-    className='inner'
-  />
-
-  <TextField
-    id='outlined-basic'
-    label='Zip'
-    variant='outlined'
-    value={zip}
-    onChange={(e) => setZip(e.target.value)}
-    className='inner'
-  />
-</div>
-
-
-
-<button onClick={onSubmit} > On submit </button>
-
-
-
-
-
-    </>
-
-
-
+      <Button variant="contained" onClick={onSubmit}>
+        Sign Up
+      </Button>
+    </div>
   );
 };
 
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,34 +1,27 @@
-import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
-import "./Styling/LoginForm.css";
-import { useNavigate } from "react-router-dom";
-import "./Styling/Account.css";
+import React, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import './Styling/LoginForm.css';
+import { useNavigate } from 'react-router-dom';
+import ShippingInfo from '../components/ShippingInfo';
 // import {  createUserWithEmailAndPassword  } from 'firebase/auth';
 // import {auth} from '../firebase';
 
+
 const Account = () => {
   const navigate = useNavigate();
-  const isLoggedIn = sessionStorage.getItem("isLoggedIn");
-  const password = sessionStorage.getItem("password");
-  const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
+  const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+  const password = sessionStorage.getItem('password');
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
 
   const handlePasswordSubmit = (event) => {
     event.preventDefault();
     if (password === oldPassword) {
-      alert("Password Changed");
-      sessionStorage.setItem("password", newPassword);
+      alert('Password Changed');
+      sessionStorage.setItem('password', newPassword)
     } else {
-      alert("Password Incorrect");
+      alert('Password Incorrect');
     }
-  };
-
-  const errorPasswordIncorrect = () => {
-    alert("Password Incorrect");
-  };
-
-  const errorFieldBlank = (field) => {
-    alert(field + " cannot be blank");
   };
 
   const handleSignOut = () => {
@@ -48,15 +41,25 @@ const Account = () => {
 
 
   };
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleDisplayInfo = () => {
+    console.log(sessionStorage.getItem('firstName'));
+    console.log(sessionStorage.getItem('lastName'));
+    console.log(sessionStorage.getItem('address'));
+    console.log(sessionStorage.getItem('city'));
+    console.log(sessionStorage.getItem('state'));
+    console.log(sessionStorage.getItem('zip'));
+    console.log(sessionStorage.getItem('password'));
+  };
+
   const style_outer = {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around',
-  };
-
-  
-  const handleLoginClick = () => {
-    navigate("/login");
   };
 
   return (
@@ -66,10 +69,10 @@ const Account = () => {
           <div className='changePasswordContainer'>
             <h1>Change Password</h1>
             <TextField
-              style={{ marginBottom: "20px" }}
-              id="standard"
-              label="Old Password"
-              variant="standard"
+              style={{ marginBottom: '20px' }}
+              id='standard'
+              label='Old Password'
+              variant='standard'
               value={oldPassword}
               onChange={(e) => {
                 setOldPassword(e.target.value);
@@ -77,9 +80,9 @@ const Account = () => {
             />
             <form onSubmit={handlePasswordSubmit}>
               <TextField
-                id="standard"
-                label="New Password"
-                variant="standard"
+                id='standard'
+                label='New Password'
+                variant='standard'
                 value={newPassword}
                 onChange={(e) => {
                   setNewPassword(e.target.value);
@@ -87,78 +90,22 @@ const Account = () => {
               />
             </form>
           </div>
-          <div className="change-shipping">
-            <h1> Your Shipping Info </h1>
-            <div className="form-row">
-              <div className="form-col">
-                <TextField
-                  id="standard"
-                  label="First Name"
-                  variant="standard"
-                  value={sessionStorage.getItem("firstName")}
-                />
-                <TextField
-                  id="standard"
-                  label="Last Name"
-                  variant="standard"
-                  value={sessionStorage.getItem("lastName")}
-                />
-              </div>
-              <div className="form-row">
-                <div className="form-col">
-                  <TextField
-                    id="standard"
-                    label="Address"
-                    variant="standard"
-                    value={sessionStorage.getItem("address")}
-                  />
-                  <TextField
-                    id="standard"
-                    label="City"
-                    variant="standard"
-                    value={sessionStorage.getItem("city")}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-col">
-                <TextField
-                  id="standard"
-                  label="State"
-                  variant="standard"
-                  value={sessionStorage.getItem("state")}
-                />
-                <TextField
-                  id="standard"
-                  label="Zip"
-                  variant="standard"
-                  value={sessionStorage.getItem("zip")}
-                />
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-col">
-                <TextField
-                  id="standard"
-                  label="Credit Card"
-                  variant="standard"
-                  value={sessionStorage.getItem("creditCard")}
-                />
-              </div>{" "}
-            </div>
+          <div className='changeShippingContainer'>
+            <h1> Your shipping info is </h1>
+            <ShippingInfo />
           </div>
-          <div className="sign-out-button">
+          <div>
             <h1> Sign Out</h1>
-            <Button variant="contained" onClick={handleSignOut}>
+            <Button variant='contained' onClick={handleSignOut}>
               Sign Out
             </Button>
+
           </div>
         </div>
       ) : (
-        <div className="not-logged-in">
+        <div>
           <h1> You are not logged in</h1>
-          <Button variant="contained" onClick={handleLoginClick}>
+          <Button variant='contained' onClick={handleLoginClick}>
             Login
           </Button>
         </div>
